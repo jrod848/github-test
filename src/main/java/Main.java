fcb2a32c3e17dcb2714e7f2ef4dd658e58d6f425
Jorge R <105378996+jrod848@users.noreply.github.com>
2023-06-20 10:07:14 -0400

    Added exclamation marks.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Another print line");
-        System.out.println("Another print line");
+        System.out.println("Another print line!");
+        System.out.println("Another print line!");
     }
 }
